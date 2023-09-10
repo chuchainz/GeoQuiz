e8fc5a0fdc8e625d54e7c81864cb8307212dde2c
Jimmy Chu <chuchainz@outlook.com>
2023-09-10 14:40:14 -0700

    first push w/o challenges

---
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizViewModel.kt
@@ -0,0 +1,36 @@
+import android.util.Log
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.bignerdranch.android.geoquiz.R
+
+private const val TAG = "QuizViewModel"
+const val CURRENT_INDEX_KEY = "CURRENT_INDEX_KEY"
+const val IS_CHEATER_KEY = "IS_CHEATER_KEY"
+
+class QuizViewModel (private val savedStateHandle: SavedStateHandle) : ViewModel() {
+    private val questionBank = listOf(
+        Question(R.string.question_pikachu, true),
+        Question(R.string.question_ash, false),
+        Question(R.string.question_rayquaza, true),
+        Question(R.string.question_gengar, false),
+        Question(R.string.question_mew, true),
+        Question(R.string.question_jesse, false)
+    )
+
+    var isCheater: Boolean
+        get() = savedStateHandle.get(IS_CHEATER_KEY) ?: false
+        set(value) = savedStateHandle.set(IS_CHEATER_KEY, value)
+
+    private var currentIndex: Int
+        get() = savedStateHandle.get(CURRENT_INDEX_KEY) ?: 0
+        set(value) = savedStateHandle.set(CURRENT_INDEX_KEY, value)
+
+    val currentQuestionAnswer: Boolean
+        get() = questionBank[currentIndex].answer
+    val currentQuestionText: Int
+        get() = questionBank[currentIndex].textResId
+
+    fun moveToNext() {
+        currentIndex = (currentIndex + 1) % questionBank.size
+    }
+}
